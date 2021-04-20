2eedf1b4551a24e60defd3291c5d94144611c0a0
steveSnyder <74982034+stevesnyder@users.noreply.github.com>
2021-04-20 02:52:14 -0400

    wrote writeFile and init functions

--- index.js
+++ index.js
@@ -1,13 +1,79 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generateMarkdown = require('./utils/generateMarkdown');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'username',
+        message: 'What is your GitHub username? ',
+      },
+      {
+        type: 'input',
+        name: 'email',
+        message: 'What is your email address? ',
+      },
+      {
+        type: 'input',
+        name: 'projectName',
+        message: 'What is your project\'s name? ',
+      },
+      {
+        type: 'input',
+        name: 'description',
+        message: 'Please write a short description of your project: ',
+      },
+      {
+        type: 'list',
+        name: 'license',        
+        message: 'What kind of license should your project have?',
+        choices: ['MIT', 'APACHE 2.0', 'GPL 3.0', 'BSD 3', 'None'],
+      },
+      {
+        type: 'input',
+        name: 'dependencies',
+        message: 'What command should be run to install dependencies? ',
+        default: 'npm i',
+      },
+      {
+        type: 'input',
+        name: 'tests',
+        message: 'What command should be run to run tests? ',
+        default: 'npm test',
+      },
+      {
+        type: 'input',
+        name: 'repoInfo',
+        message: 'What does the user need to know about using the repo? ',
+      },
+      {
+        type: 'input',
+        name: 'repoContributions',
+        message: 'What does the user need to know about contributing to the repo? ',
+      },
+
+
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+
+  fs.writeFile(fileName, data, (err) => {
+    if (err) {
+        console.log(err);
+    }
+    console.log('README.md generated successfully');
+});
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions).then((res) => {
+       writeToFile('README.md', generateMarkdown({...res}));
+    });
+}
 
 // Function call to initialize app
 init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,19 +1,71 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  
+// ![badge](https://img.shields.io/badge/license-${questions.license}-brightgreen)
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
 function renderLicenseLink(license) {}
 
+// 'https://opensource.org/licenses/Apache-2.0'
+// 'https://opensource.org/licenses/MIT'
+// 'https://www.gnu.org/licenses/gpl-3.0'
+// 'https://opensource.org/licenses/BSD-3-Clause'
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  // if (license == 'None'){
+  //   return '';
+  // }
+
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return `# ${data.projectName}
 
+  Developed by: ${data.username}
+
+  ## Table of Contents
+
+  - [About the Project](#about-the-project)
+  - [Important Commands](#important-commands)
+  - [About the Repo](#about-the-repo)
+  - [Contact](#contact)
+
+  ## About the Project
+
+  ${data.description}
+
+  ## Important Commands
+
+  ### To install dependencies: 
+
+  ${data.dependencies}
+
+  ### To run tests:
+
+  ${data.tests}
+
+  ## About the Repo
+
+  ### Using the Repo
+
+  ${data.repoInfo}
+
+  ### Contributing to the Repo
+
+  ${data.repoContributions}
+
+  ## License
+
+  This project is licensed under <a href=${renderLicenseLink(data.license)}>${data.license}</a>
+
+  ### Contact
+
+  Contact ${data.email} with any questions or concerns.
 `;
 }
 
